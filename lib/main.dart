247b70d0d0218e9ed1e90220adcd50640abf4a95
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-03-18 12:19:52 +0600

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:test_block/cubit/counter_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,16 +10,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
+    return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const MyHomePage(),
-      ),
-    );
+        home: const MyHomePage());
   }
 }
 
@@ -31,63 +25,10 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if (state.wasIncriment == true) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                duration: Duration(milliseconds: 5),
-                content: Text('Incriment')));
-          } else {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                duration: Duration(milliseconds: 5),
-                content: Text('Decriment')));
-          }
-          // TODO: implement listener
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              BlocBuilder<CounterCubit, CounterState>(
-                builder: (context, state) {
-                  return Text(
-                    "The number is :${state.value}",
-                    style: const TextStyle(
-                        fontSize: 30,
-                        color: Colors.red,
-                        fontWeight: FontWeight.w600),
-                  );
-                },
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).incriment();
-                      },
-                      child: const Icon(Icons.add)),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).decriment();
-                      },
-                      child: const Icon(Icons.remove)),
-                ],
-              )
-            ],
-          ),
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        appBar: AppBar(),
+        body: const Center(
+          child: Text("This is demo"),
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
